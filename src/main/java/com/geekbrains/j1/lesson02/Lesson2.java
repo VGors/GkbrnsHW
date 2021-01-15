35b9f886851b23152f080f3a6ee133fb4dd578c0
Victor <v.gorshenev@gmail.com>
2021-01-15 16:43:35 +0500

    Geekbrains Lesson1 home work

---
+++ src/main/java/com/geekbrains/j1/lesson02/Lesson2.java
@@ -0,0 +1,25 @@
+package com.geekbrains.j1.lesson02;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Lesson2 {
+    public static void main (String[] args){
+        //Task 1
+
+        //Task 2
+
+        //Task 3
+
+        //Task 4
+
+        //Task 5
+    }
+
+    //Task6
+    public static boolean chekBalance(int[] inputArray){
+        return true;
+    }
+
+    //Task 7
+}
