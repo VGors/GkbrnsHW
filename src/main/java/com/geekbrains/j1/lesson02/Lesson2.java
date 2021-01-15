104a01831dff754e63f212cea0cc3b259053eec8
Victor <v.gorshenev@gmail.com>
2021-01-15 20:35:18 +0500

    Geekbrains Lesson1 home work

--- src/main/java/com/geekbrains/j1/lesson02/Lesson2.java
+++ src/main/java/com/geekbrains/j1/lesson02/Lesson2.java
@@ -6,20 +6,39 @@ import java.util.Arrays;
 public class Lesson2 {
     public static void main (String[] args){
         //Task 1
+        int counter = 0;
+        byte[] bitArray = {0, 0, 1, 0, 1, 1, 1, 0};
+        for (byte i: bitArray) {
+            if (i == 0){
+                bitArray[counter] = 1;
+            }
+            else {
+                bitArray[counter] = 0;
+            }
+            counter++;
+        }
 
         //Task 2
+        int[] emptyArray = new int[8];
+        for (int i = 0; i < emptyArray.length; i++) {
+            emptyArray[i] = i * 3;
+        }
 
         //Task 3
+
 
         //Task 4
 
         //Task 5
     }
 
-    //Task6
+    //Task 6
     public static boolean chekBalance(int[] inputArray){
         return true;
     }
 
     //Task 7
+    public static void moveArray(int[] inputArray, int n){
+
+    }
 }
