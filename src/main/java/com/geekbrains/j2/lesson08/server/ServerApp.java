1f2e129700cf193d41967f21f48b02d939d3e9f6
Victor <v.gorshenev@gmail.com>
2021-03-15 02:05:22 +0300

    some changes

--- src/main/java/com/geekbrains/j2/lesson08/server/ServerApp.java
+++ src/main/java/com/geekbrains/j2/lesson08/server/ServerApp.java
@@ -5,6 +5,9 @@ import com.geekbrains.j2.lesson08.server.services.ServerEngine;
 
 public class ServerApp {
     public static void main(String[] args) {
-        new ServerEngine();
+        Object object = new Integer(10);
+        String string = (String) object;
+        System.out.println(string);
+//        new ServerEngine();
     }
 }
